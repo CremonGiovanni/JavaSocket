d4b612d69c757630b680602d16e65a5bfcbb6c8d
Cremon Giovanni <cremon.giovanni@osdbsesto>
2022-02-16 09:34:46 +0100

    add server and client

--- ClientSocket/src/clientsocket/ClientSocket.java
+++ ClientSocket/src/clientsocket/ClientSocket.java
@@ -1,21 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package clientsocket;
-
-/**
- *
- * @author cremon.giovanni
- */
+import java.io.*;
+import java.net.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 public class ClientSocket {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+        Socket clientSocket = new Socket("192.168.56.1", 5000);
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+   
+         out.println("hello server");
+        String resp = in.readLine();
+       System.out.println("risposta del server: " + resp);
+        
+        in.close();
+        out.close();
+        clientSocket.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ClientSocket.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
+
 }
---
+++ JavaApplication20/src/Server/Server.java
@@ -0,0 +1,32 @@
+package Server;
+
+import java.io.*;
+import java.net.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Server {
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(5000);
+
+            Socket clientSocket = serverSocket.accept();
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String greeting = in.readLine();
+            if ("hello server".equals(greeting)) {
+                out.println("hello client");
+            } else {
+                out.println("unrecognised greeting");
+            }
+            in.close();
+            out.close();
+            clientSocket.close();
+            serverSocket.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
